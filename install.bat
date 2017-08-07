5ac70d1ba35ed16def2dc8f8f44aa1565b3dc3fa
Ian Walter <public@iankwalter.com>
2017-08-07 12:19:42 -0400

    Adding commitizen installation

--- install.bat
+++ install.bat
@@ -3,5 +3,5 @@ CALL xcopy /y /f .gitconfig %HOMEPATH%\.gitconfig
 
 :: Install diff-so fancy diffing tool if NPM is installed.
 npm version >nul 2>&1 && (
-  CALL npm install -g diff-so-fancy
+  CALL npm install -g diff-so-fancy commitizen
 )
