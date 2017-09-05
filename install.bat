6d49556e57ed339527e4e9132b27ab73276ced9f
Ian Walter <public@iankwalter.com>
2017-09-04 20:10:58 -0400

    Removing batch file
    Using Cygwin on Windows now.

--- install.bat
+++
@@ -1,7 +0,0 @@
-:: Copy configutation file.
-CALL xcopy /y /f .gitconfig %HOMEPATH%\.gitconfig
-
-:: Install diff-so fancy diffing tool if NPM is installed.
-npm version >nul 2>&1 && (
-  CALL npm install -g diff-so-fancy commitizen
-)
