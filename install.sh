acec24c7400ef8126dbf51e5c9099b24bfea862c
Ian Walter <public@iankwalter.com>
2018-09-29 13:27:08 -0400

    Removing diff-so-fancy install since it will be done with dotnpm

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Install diff-so-fancy diffing tool if NPM is installed.
-if [[ `which npm` ]]; then
-  npm install -g diff-so-fancy
-fi
-
 cp .gitconfig ~/.gitconfig
 
 if [[ $? == 0 ]]; then
